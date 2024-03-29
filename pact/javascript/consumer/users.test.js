4d1ace9f9d09d8e2c49cac71fd53654dc448ea66
Luis Lira <luis_ssbm@hotmail.com>
2024-03-29 14:08:05 -0600

    sing in / sign up tests

---
+++ pact/javascript/consumer/users.test.js
@@ -0,0 +1,78 @@
+const { PactV3, Matchers } = require('@pact-foundation/pact')
+const axios = require('axios')
+const path = require('path')
+const { like, string, integer } = Matchers
+const { fromProviderState } = require("@pact-foundation/pact/src/v3/matchers");
+
+
+const pact = new PactV3({
+  dir: path.resolve(process.cwd(), '../pacts'),
+  consumer: 'UsersConsumer',
+  provider: 'FastAPIProvider',
+})
+
+const userData = {
+  token: string(),
+  user: like({
+    id: integer(),
+    email: string()
+  })
+}
+
+describe('Users consumer', () => {
+  // it('should return new user data', () => {
+  //   pact
+  //     .given('A new user with random number')
+  //     .uponReceiving('A request to create a new user')
+  //     .withRequest({
+  //       method: 'POST',
+  //       path: '/signup/',
+  //       body: {
+  //         email: fromProviderState('email-${randomNumber}@email.com', 'email-123456@email.com'),
+  //         password: '123456',
+  //       }
+  //     })
+  //     .willRespondWith({
+  //       status: 201,
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //       },
+  //       body: {
+  //         data: userData
+  //       }
+  //     })
+  //
+  //   return pact.executeTest(async (mockServer) => {
+  //     const data = { email: 'email-123456@email.com', password: '123456' }
+  //     await axios.post(`${mockServer.url}/signup/`, data)
+  //   })
+  // })
+
+  it('should return user data after sign in', () => {
+    pact
+      .given('User already exists')
+      .uponReceiving('A request to sign in')
+      .withRequest({
+        method: 'POST',
+        path: '/signin/',
+        body: {
+          email: fromProviderState('${email}', 'email@email.com'),
+          password: fromProviderState('${password}', '123456')
+        }
+      })
+      .willRespondWith({
+        status: 200,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: {
+          data: userData
+        }
+      })
+
+    return pact.executeTest(async (mockServer) => {
+      const data = { email: 'email@email.com', password: '123456' }
+      await axios.post(`${mockServer.url}/signin/`, data)
+    })
+  })
+});
--- pact/javascript/provider/provider.test.js
+++ pact/javascript/provider/provider.test.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const { Verifier } = require('@pact-foundation/pact')
+const axios = require('axios')
 
 describe('Pact Verification', () => {
   it('should validate the pact', () => {
@@ -7,7 +8,8 @@ describe('Pact Verification', () => {
       providerBaseUrl: 'http://localhost:8000',
       pactUrls: [
         // path.resolve(process.cwd(), '../pacts/HelloWorldConsumer-FastAPIProvider.json'),
-        path.resolve(process.cwd(), '../pacts/GameConsumer-FastAPIProvider.json')
+        // path.resolve(process.cwd(), '../pacts/GameConsumer-FastAPIProvider.json')
+        path.resolve(process.cwd(), '../pacts/UsersConsumer-FastAPIProvider.json')
       ],
       stateHandlers: {
         'A logged user with a valid token': () => {
@@ -15,7 +17,24 @@ describe('Pact Verification', () => {
           return Promise.resolve({
             token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiZXhwIjoxNzExNzc3NDUzfQ.zh5PIoLumjj0jG5c4MfhX7uJHso9MG8Zb3Aslfk-Tfw'
           })
-        }
+        },
+        'A new user with random number': () => {
+          const randomNumber = Math.floor(Math.random() * 100000)
+          return Promise.resolve({ randomNumber })
+        },
+        'User already exists': async () => {
+          const randomNumber = Math.floor(Math.random() * 100000)
+          const data = {
+            email: `email-provider-${randomNumber}@email.com`,
+            password: '123456'
+          }
+          await axios.post('http://localhost:8000/signup/', data)
+
+          return Promise.resolve({
+            email: data.email,
+            password: data.password
+          })
+        },
       }
     })
 
